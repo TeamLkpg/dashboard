4c8253d933a6a0b6f5e10a10ce4082c186d16823
Johanna <152695438+Johanna1066@users.noreply.github.com>
2024-11-11 14:43:57 +0100

    Add styling to time-slider Make setDataset to take time aswell as location to choose dataset

--- script.js
+++ script.js
@@ -1,159 +1,13 @@
 const ctx = document.getElementById("chart");
 
-//const recordID = [];
-const month = [];
-const day = [];
-const year = [];
-const avgTempFahr = [];
-//const avgTempUncFahr = [];
-const city = [];
-//const countryID = [];
-const country = [];
-//const latitude = [];
-//const longitude = [];
-const date = [];
 
-var datasets = [];
-
-var selection = [];
-
-
-
-//WORKING CODE
-
-/*
-const response = fetch("tmp.csv")
-  .then((response) => response.text())
-  .then((response) => papaParseJson(response))
-  .then((response) => {
-    createNewData("Ukraine");
-    createNewData("New Zealand");
-    createChart(datasets);
-  });
-
-function createNewData(location) {
-  var datas = [];
-  for (let i = 0; i < country.length; i++) {
-    if (country[i] == location) {
-      data = avgTempFahr[i];
-      datas.push(data);
-    }
-  }
-  datasets.push(datas);
-}
-
-function createChart(input) {
-  new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: date,
-      datasets: [
-        //Make funktion to create this instead
-        {
-          label: "Ukraine",
-          data: input[0],
-          backgroundColor: "#00fff0",
-          borderWidth: 3,
-          tension: 0.1,
-        },
-        {
-          label: "New Zealand",
-          data: input[1],
-          backgroundColor: "#002351",
-          borderWidth: 3,
-          tension: 0.1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
-}
-
-function papaParseJson(tmpCsvData) {
-  Papa.parse(tmpCsvData, {
-    header: true,
-    complete: function (results) {
-      //console.log("Finished:", results.data);
-      for (let i = 0; i < results.data.length; i++) {
-        //Kanske kan bli fel eftersom month inte är header
-        //recordID.push(results.data[i].record_id)
-        month.push(results.data[i].month);
-        day.push(results.data[i].day);
-        year.push(results.data[i].year);
-        date.push(
-          results.data[i].day +
-            "-" +
-            results.data[i].month +
-            "-" +
-            results.data[i].year
-        );
-        avgTempFahr.push(results.data[i].AverageTemperatureFahr);
-        //avgTempUncFahr.push(results.data[i].AverageTemperatureUncertaintyFahr)
-        city.push(results.data[i].City);
-        //countryID.push(results.data[i].country_id)
-        country.push(results.data[i].Country);
-        //latitude.push(results.data[i].Latitude)
-        //longitude.push(results.data[i].Longitude)
-      }
-    },
-  });
-}
-*/
-
-//END WORKING CODE
-
-/*
-function csvToJson(csvString) {
-    const rows = csvString
-        .split("\n");
-
-    const headers = rows[0]
-        .split(",");
-
-    const jsonData = [];
-    for (let i = 1; i < rows.length; i++) {
-
-        const values = rows[i]
-            .split(",");
-
-        const obj = {};
-
-        for (let j = 0; j < headers.length; j++) {
-
-            const key = headers[j]
-                .trim();
-            const value = values[j]
-                .trim();
-
-            obj[key] = value;
-        }
-
-        jsonData.push(obj);
-    }
-    return JSON.stringify(jsonData);
-}
-
-*/
-
-/*
-document.getElementById('fileInput').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const content = e.target.result;
-            document.getElementById('output').innerText = content;
-        };
-        reader.readAsText(file);
-    }
-});
-*/
+//Timespan Slider values
+const startTimeInput = document.getElementById("startTime");
+const endTimeInput = document.getElementById("endTime");
+const displayLowTimeValue = document.getElementById("display-low-value");
+const displayHighTimeValue = document.getElementById("display-high-value");
+const lowerSlider = document.getElementById("lower");
+const upperSlider = document.getElementById("upper");
 
 /*Testar map*/
 
@@ -175,83 +29,90 @@ const tottoriData = new Map();
 const warsawData = new Map();
 const wroclawData = new Map();
 
+const countryArray = [
+  "New Zealand","New Zealand",
+  "Ukraine","Ukraine","Ukraine",
+  "Brazil","Brazil",
+  "South Africa","South Africa",
+  "France","France",
+  "Sweden","Sweden",
+  "Japan","Japan",
+  "Poland","Poland",
+];
 
-const countryArray = ["New Zealand", "New Zealand", 
-                      "Ukraine", "Ukraine", "Ukraine", 
-                      "Brazil", "Brazil", 
-                      "South Africa", "South Africa",
-                      "France", "France",
-                      "Sweden", "Sweden",
-                      "Japan", "Japan",
-                      "Poland", "Poland"];
+const cityArray = [
+  "Hamilton","Auckland",
+  "Kiev","Lvov","Odessa",
+  "Brasília","Canoas",
+  "Cape Town","Johannesburg",
+  "Paris","Marseille",
+  "Stockholm","Uppsala",
+  "Tokyo","Tottori",
+  "Warsaw","Wroclaw",
+];
 
-const cityArray =   ["Hamilton", "Auckland", 
-                      "Kiev", "Lvov", "Odessa", 
-                      "Brasília", "Canoas", 
-                      "Cape Town", "Johannesburg",
-                      "Paris", "Marseille",
-                      "Stockholm", "Uppsala",
-                      "Tokyo", "Tottori",
-                      "Warsaw", "Wroclaw"];
-                    
-const dataArray =   [hamiltonData, aucklandData, 
-                      kievData, lvovData, odessaData, 
-                      brasiliaData, canoasData, 
-                      capeTownData, johannesburgData,
-                      parisData, marseilleData,
-                      stockholmData, uppsalaData,
-                      tokyoData, tottoriData,
-                      warsawData, wroclawData];
+const dataArray = [
+  hamiltonData, aucklandData,
+  kievData, lvovData, odessaData,
+  brasiliaData, canoasData,
+  capeTownData, johannesburgData,
+  parisData, marseilleData,
+  stockholmData, uppsalaData,
+  tokyoData, tottoriData,
+  warsawData, wroclawData,
+];
 
+var selectionLocations = ["Hamilton"]; //array of cities selected
+var selectionTime = [lowerSlider.value, upperSlider.value]; //min to map
+var selection = [];
 
+var datasets = [];
 
-var selectionLocations = [];  //array of cities selected
-var selectionTime = [];       //min to max
+const response2 = fetch("temperature.csv")
+  .then((response2) => response2.text())
+  .then((response2) => papaParseJsonMap(response2))
+  .then((response2) => setDataset())
+  .then((response2) => createChart2(datasets[0]));
 
-var datasets = [];            
-
-const response2 = fetch("tmp2.csv")
-    .then((response2) => response2.text())
-    .then((response2) => papaParseJsonMap(response2))
-    .then((response2) => createChart2(hamiltonData));
-   
-
-function setDataset(selectionLocations) {
-  for (let i = 0; i < cityArray.length; i++) {
-    if (city[i] == selectionLocations) {
-      datasets.push(dataArray[i]);
-    }
-  }
-  datasets.push(datas);
-}
 
 function papaParseJsonMap(tmpCsvData) {
   Papa.parse(tmpCsvData, {
     header: true,
     complete: function (results) {
-      //console.log("Finished:", results.data);
       for (let i = 0; i < results.data.length; i++) {
-        if (results.data[i].City === "Hamilton") {
-          hamiltonData.set(
-            results.data[i].year + "/" + results.data[i].month,
-            results.data[i].AverageTemperatureFahr
-          );
-        }
-        if (results.data[i].City === "Auckland") {
-          aucklandData.set(
-            results.data[i].year + "/" + results.data[i].month,
-            results.data[i].AverageTemperatureFahr
-          );
-        }
+        cityArray.forEach((city) => {
+          if (results.data[i].City === city) {
+            dataArray[cityArray.indexOf(city)].set(
+              results.data[i].year + "/" + results.data[i].month,
+              results.data[i].AverageTemperatureFahr
+            );
+          }
+        });
       }
-      console.log(results.data[0].Country);
     },
   });
 }
 
+function setDataset() {
+  for (let i = 0; i < cityArray.length; i++) {
+    if (cityArray[i] == selectionLocations) {
+      let temp = new Map();
+      console.log(dataArray[i].size);
+      for (let [key, value] of dataArray[i]) {
+        let year=parseInt(key[0]+key[1]+key[2]+key[3]);
+        console.log(year);
+        if (year >= selectionTime[0] && year <= selectionTime[1]) {
+          temp.set(key, value);
+        }
+      }
+      datasets.push(temp);
+    }
+  }
+}
+
 function createChart2(input) {
-    const labels = Array.from(input.keys());
-    const data = Array.from(input.values());
+  const labels = Array.from(input.keys());
+  const data = Array.from(input.values());
 
   new Chart(ctx, {
     type: "line",
@@ -311,10 +172,9 @@ NZTowncheckboxes.forEach((checkbox) => {
 */
 //End script for checkboxes
 
-
 function toggleShow() {
   const dropdown = document.getElementById("hiddenCalendar");
-  const button = event.target; 
+  const button = event.target;
 
   dropdown.classList.toggle("show");
 
@@ -324,78 +184,44 @@ function toggleShow() {
   dropdown.style.left = `${rect.left + window.scrollX}px`;
 }
 
-//Timespan Slider
-const startTimeInput = document.getElementById('startTime');
-const endTimeInput = document.getElementById('endTime');
-const displayLowTimeValue = document.getElementById('display-low-value');
-const displayHighTimeValue = document.getElementById('display-high-value');
-const lowerSlider = document.getElementById('lower');
-const upperSlider = document.getElementById('upper');
-
-function updateYearSpan(){
-    const startValue = startTimeInput.value || "1870";
-    const endValue = endTimeInput.value || "2000";
-
-    if(1850 <= startValue && startValue <= 2020){
-      displayLowTimeValue.textContent = startValue;
-      lowerSlider.value = startValue;
-    }
-    if(1850 <= endValue && endValue <= 2020){
-      displayHighTimeValue.textContent = endValue;
-      upperSlider.value = endValue;
-    }
-}
-
 let labelArray = [];
 
-function createDynamicCheckbox(){
-  const locationBox = document.getElementById('checkLocationBox');
- 
-  
+function createDynamicCheckbox() {
+  const locationBox = document.getElementById("checkLocationBox");
 
   let lastCountry = "start";
-  for(let i = 0; i < countryArray.length; i++){
-    let myLabel = document.createElement('label');
-    let myCheckbox = document.createElement('input');
-    myCheckbox.type = 'checkbox';
+  for (let i = 0; i < countryArray.length; i++) {
+    let myLabel = document.createElement("label");
+    let myCheckbox = document.createElement("input");
+    myCheckbox.type = "checkbox";
 
     let currentCountry = countryArray[i];
 
-    if (currentCountry != lastCountry){
-      let myCountryLabel = document.createElement('label');
-      let myCountryCheckbox = document.createElement('input');
-      myCountryCheckbox.type = 'checkbox'
-      console.log("")
-      console.log(currentCountry + " " + i)
-      myCountryLabel.className = 'containerLand';
+    if (currentCountry != lastCountry) {
+      let myCountryLabel = document.createElement("label");
+      let myCountryCheckbox = document.createElement("input");
+      myCountryCheckbox.type = "checkbox";
+      myCountryLabel.className = "containerLand";
       myCountryCheckbox.id = currentCountry;
       myCountryCheckbox.value = currentCountry;
       myCountryLabel.appendChild(myCountryCheckbox);
       myCountryLabel.innerHTML += countryArray[i];
       labelArray.push(myCountryLabel);
       lastCountry = currentCountry;
-
     }
-    
-    console.log(cityArray[i] + " " + i)
-    myLabel.className = 'containerStad';
+    myLabel.className = "containerStad";
     myCheckbox.id = cityArray[i];
     myCheckbox.value = cityArray[i];
-    
+
     myLabel.appendChild(myCheckbox);
     myLabel.innerHTML += cityArray[i];
     labelArray.push(myLabel);
-    
-
-    
   }
-  
-  labelArray.forEach((checkbox) =>{
+
+  labelArray.forEach((checkbox) => {
     locationBox.appendChild(checkbox);
   });
 }
-
-  
 createDynamicCheckbox();
 
 /*
@@ -404,12 +230,25 @@ const cityArray = ["Hamilton", "Auckland"];
 const dataArray = [hamiltonData, aucklandData];
 */
 
+function updateYearSpan() {
+  const startValue = startTimeInput.value || "1870";
+  const endValue = endTimeInput.value || "2000";
 
-function updateSliderValues(){
+  if (1850 <= startValue && startValue <= 2020) {
+    displayLowTimeValue.textContent = startValue;
+    lowerSlider.value = startValue;
+  }
+  if (1850 <= endValue && endValue <= 2020) {
+    displayHighTimeValue.textContent = endValue;
+    upperSlider.value = endValue;
+  }
+}
+
+function updateSliderValues() {
   const startValue = parseInt(lowerSlider.value);
   const endValue = parseInt(upperSlider.value);
 
-  if(startValue <= endValue){
+  if (startValue <= endValue) {
     displayLowTimeValue.textContent = startValue;
     displayHighTimeValue.textContent = endValue;
 
@@ -418,8 +257,7 @@ function updateSliderValues(){
   }
 }
 
-startTimeInput.addEventListener('input', updateYearSpan);
-endTimeInput.addEventListener('input', updateYearSpan);
-lowerSlider.addEventListener('input', updateSliderValues);
-upperSlider.addEventListener('input', updateSliderValues);
-
+startTimeInput.addEventListener("input", updateYearSpan);
+endTimeInput.addEventListener("input", updateYearSpan);
+lowerSlider.addEventListener("input", updateSliderValues);
+upperSlider.addEventListener("input", updateSliderValues);
