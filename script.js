8c54b323880144f74e2c5f87d516b7cf0595137f
Johanna <152695438+Johanna1066@users.noreply.github.com>
2024-11-11 17:01:49 +0100

    Add reading from time-sliders
    Co-authored-
    
    by: Oskar Jonsson <ojonss@users.noreply.github.com>
    Co-authored-by: dragodror <dragodror@users.noreply.github.com>

--- script.js
+++ script.js
@@ -62,17 +62,18 @@ const dataArray = [
   warsawData, wroclawData,
 ];
 
-var selectionLocations = ["Hamilton"]; //array of cities selected
-var selectionTime = [lowerSlider.value, upperSlider.value]; //min to map
+var selectionLocations = ["Hamilton", "Auckland"]; //array of cities selected
+var selectionTime = [parseInt(lowerSlider.value), parseInt(upperSlider.value)]; //min to map
 var selection = [];
 
 var datasets = [];
+var datasetsCity = [];
 
 const response2 = fetch("temperature.csv")
   .then((response2) => response2.text())
   .then((response2) => papaParseJsonMap(response2))
   .then((response2) => setDataset())
-  .then((response2) => createChart2(datasets[0]));
+  .then((response2) => createChartForMaps(datasets));
 
 
 function papaParseJsonMap(tmpCsvData) {
@@ -82,6 +83,7 @@ function papaParseJsonMap(tmpCsvData) {
       for (let i = 0; i < results.data.length; i++) {
         cityArray.forEach((city) => {
           if (results.data[i].City === city) {
+            //let AverageTemperatureCelsius = (results.data[i].AverageTemperatureFahr - 32) * 5 / 9;
             dataArray[cityArray.indexOf(city)].set(
               results.data[i].year + "/" + results.data[i].month,
               results.data[i].AverageTemperatureFahr
@@ -94,27 +96,33 @@ function papaParseJsonMap(tmpCsvData) {
 }
 
 function setDataset() {
-  for (let i = 0; i < cityArray.length; i++) {
-    if (cityArray[i] == selectionLocations) {
-      let temp = new Map();
-      console.log(dataArray[i].size);
-      for (let [key, value] of dataArray[i]) {
-        let year=parseInt(key[0]+key[1]+key[2]+key[3]);
-        console.log(year);
-        if (year >= selectionTime[0] && year <= selectionTime[1]) {
-          temp.set(key, value);
+  datasets = [];
+  datasetsCity = [];
+  
+  for (var city of selectionLocations) {
+    for (let i = 0; i < cityArray.length; i++) {
+      if (cityArray[i] == city) {
+        let temp = new Map();
+        for (let [key, value] of dataArray[i]) {
+          let year = parseInt(key[0] + key[1] + key[2] + key[3]);
+          if (year >= selectionTime[0] && year <= selectionTime[1]) {
+            temp.set(key, value);
+          }
         }
+        datasets.push(temp);
+        datasetsCity.push(city);
       }
-      datasets.push(temp);
     }
   }
 }
 
-function createChart2(input) {
+let myChart;
+
+/*
+function createChart(input) {
   const labels = Array.from(input.keys());
   const data = Array.from(input.values());
-
-  new Chart(ctx, {
+   myChart =new Chart(ctx, {
     type: "line",
     data: {
       labels: labels,
@@ -123,53 +131,75 @@ function createChart2(input) {
           label: "Hamilton",
           data: data,
           backgroundColor: "#00fff0",
-          borderWidth: 3,
+          borderColor: "#00fff0",
+          borderWidth: 1,
           tension: 0.1,
-        },
+          pointRadius: null, 
+        }
       ],
     },
     options: {
+      
       scales: {
         y: {
-          beginAtZero: true,
         },
       },
     },
   });
-}
+}*/
 
-//Script for checkboxes
-//For now it's only New Zealand, is it possible to make it dynamic?
-/*
-const NZcheckbox = document.getElementById("New Zealand");
-
-const NZTowncheckboxes = [
-  document.getElementById("Auckland"),
-  document.getElementById("Hamilton"),
+const graphColors = [
+  "#FF5733", // Red-Orange
+  "#33FF57", // Green
+  "#3357FF", // Blue
+  "#FF33A6", // Pink
+  "#FFD700", // Gold
+  "#4B0082", // Indigo
+  "#FF6347", // Tomato
+  "#40E0D0", // Turquoise
+  "#FF1493", // Deep Pink
+  "#20B2AA", // Light Sea Green
+  "#FFD700", // Gold
+  "#8A2BE2", // Blue Violet
+  "#A52A2A", // Brown
+  "#7FFF00", // Chartreuse
+  "#D2691E", // Chocolate
+  "#6495ED", // Cornflower Blue
+  "#FF4500"  // Orange Red
 ];
 
-NZcheckbox.addEventListener("change", function () {
-  if (NZcheckbox.checked) {
-    NZTowncheckboxes.forEach((checkbox) => {
-      checkbox.checked = true;
-    });
-  } else {
-    NZTowncheckboxes.forEach((checkbox) => {
-      checkbox.checked = false;
-    });
-  }
-});
-
-NZTowncheckboxes.forEach((checkbox) => {
-  checkbox.addEventListener("change", function () {
-    if (NZTowncheckboxes.every((checkbox) => checkbox.checked)) {
-      NZcheckbox.checked = true;
-    } else {
-      NZcheckbox.checked = false;
+function createChartForMaps(input) { //Update this function name
+  let i = -1;
+  const labels = Array.from(input[0].keys());
+  var data = input.map((element) => {
+    i++;
+    return{
+      label: datasetsCity[datasets.indexOf(element)],
+      data: Array.from(element.values()),
+      backgroundColor: graphColors[i],
+      borderColor: graphColors[i],
+      borderWidth: 1,
+      tension: 0.1,
+      pointRadius: null,
     }
   });
-});
-*/
+
+  console.log(labels);
+
+
+   myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: data,
+    },
+    options: {
+    },
+  });
+}
+
+
+
 //End script for checkboxes
 
 function toggleShow() {
@@ -222,13 +252,8 @@ function createDynamicCheckbox() {
     locationBox.appendChild(checkbox);
   });
 }
-createDynamicCheckbox();
 
-/*
-const countryArray = ["New Zealand", "New Zealand"];
-const cityArray = ["Hamilton", "Auckland"];
-const dataArray = [hamiltonData, aucklandData];
-*/
+createDynamicCheckbox();
 
 function updateYearSpan() {
   const startValue = startTimeInput.value || "1870";
@@ -237,11 +262,14 @@ function updateYearSpan() {
   if (1850 <= startValue && startValue <= 2020) {
     displayLowTimeValue.textContent = startValue;
     lowerSlider.value = startValue;
+    selectionTime = [lowerSlider.value, upperSlider.value];
   }
   if (1850 <= endValue && endValue <= 2020) {
     displayHighTimeValue.textContent = endValue;
     upperSlider.value = endValue;
+    selectionTime = [lowerSlider.value, upperSlider.value];
   }
+ 
 }
 
 function updateSliderValues() {
@@ -254,10 +282,70 @@ function updateSliderValues() {
 
     lowerSlider.value = startValue;
     upperSlider.value = endValue;
+    selectionTime = [parseInt(lowerSlider.value), parseInt(upperSlider.value)];
   }
+  
+  myChart.data.datasets = [];
+  myChart.labels = [];
+  setDataset();
+
+  // Push each dataset individually into myChart.data.datasets
+  datasets.forEach((element, index) => {
+    // Create the dataset object for each city
+    const dataset = {
+      label: datasetsCity[index],  // City name
+      data: Array.from(element.values()),  // Convert the Map's values into an array of data points
+      backgroundColor: graphColors[index % graphColors.length],  // Cycle through colors
+      borderColor: graphColors[index % graphColors.length],  // Same color for border
+      borderWidth: 1,
+      tension: 0.1,  // Line tension (for smooth curves)
+      pointRadius: null,  // Optional: Remove data points
+    };
+    myChart.data.datasets.push(dataset);
+  });
+  myChart.data.labels = Array.from(datasets[0].keys());
+  console.log(myChart.labels);
+  myChart.update();
 }
 
 startTimeInput.addEventListener("input", updateYearSpan);
 endTimeInput.addEventListener("input", updateYearSpan);
 lowerSlider.addEventListener("input", updateSliderValues);
 upperSlider.addEventListener("input", updateSliderValues);
+
+
+//Script for checkboxes
+
+//For now it's only New Zealand, is it possible to make it dynamic?
+const NZcheckbox = document.getElementById("New Zealand");
+
+const NZTowncheckboxes = [
+  document.getElementById("Auckland"),
+  document.getElementById("Hamilton"),
+];
+
+function linkCheckboxes(){
+
+}
+
+NZcheckbox.addEventListener("change", function () {
+  if (NZcheckbox.checked) {
+    NZTowncheckboxes.forEach((checkbox) => {
+      checkbox.checked = true;
+    });
+  } else {
+    NZTowncheckboxes.forEach((checkbox) => {
+      checkbox.checked = false;
+    });
+  }
+});
+
+NZTowncheckboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", function () {
+    if (NZTowncheckboxes.every((checkbox) => checkbox.checked)) {
+      NZcheckbox.checked = true;
+    } else {
+      NZcheckbox.checked = false;
+    }
+  });
+});
